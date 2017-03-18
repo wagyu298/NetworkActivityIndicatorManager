1f950cda506b2bb1861f648b4efa2692f1d42d14
wagyu298 <wagyu298@gmail.com>
2017-03-18 19:16:43 +0900

    Doxygen

--- SharedNetworkActivityIndicator/Classes/NSObject+SharedNetworkActivityIndicator.h
+++ SharedNetworkActivityIndicator/Classes/NSObject+SharedNetworkActivityIndicator.h
@@ -3,13 +3,21 @@
 
 #import <Foundation/Foundation.h>
 
+/*!
+ @brief Protocol to provide per object access to
+ [UIApplication networkActivityIndicatorVisible] instance.
+ */
 @interface NSObject (SharedNetworkActivityIndicator)
 
+/// @brief Access to networkActivityIndicatorVisible.
 @property (nonatomic,
            getter=sna_networkActivityIndicatorVisible,
            setter=sna_setNetworkActivityIndicatorVisible:) BOOL sna_networkActivityIndicatorVisible;
 
+/// @brief Same as sna_networkActivityIndicatorVisible = YES.
 - (void)sna_enableNetworkActivityIndicator;
+
+/// @brief Same as sna_networkActivityIndicatorVisible = NO.
 - (void)sna_disableNetworkActivityIndicator;
 
 @end
